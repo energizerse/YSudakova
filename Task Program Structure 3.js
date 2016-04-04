8769a6866ce933d24246464cec06b19fd7442fe6
Yauheniya Sudakova <energizerse@gmail.com>
2016-04-04 17:24:50 +0300

    Page Structure - Task3 (added code)

--- Task Program Structure 3.js
+++ Task Program Structure 3.js
@@ -1,22 +1,34 @@
 //Page Structurte - Task3
+//Default size:
+var w = 8;
+var h = 8;
+
+//Prompting size:
+q = prompt("Would you like to build the chessboard with default size(8*8)?(yes/no)", "yes");
+
+//Setting custom's size:
+if (q == "no") {
+    w = prompt("Enter weight:", 8);
+    h = prompt("Enter high:", 8);
+}
+
 //Chessboard building:
 var c = "";
-var size = prompt("Please enter size of the board:");
-for (j=1; j <=size; j ++){
-	c = "";
-    for (i = 1; i <= size; i++){
-		if (j%2 !==0) {
-			if (i%2 === 0) {
-			c += "#";
-			}
-			else {c += " ";}
-		}
-		else { 
-			if (i%2 === 0) {
-			c += " ";
-			}
-			else c += "#";
-			}	
-		}
-		console.log(c);
+for (j = 1; j <= h; j++) {
+    c = "";
+    for (i = 1; i <= w; i++) {
+        if (j % 2 !== 0) {
+            if (i % 2 === 0) {
+                c += "#";
+            }
+            else { c += " "; }
+        }
+        else {
+            if (i % 2 === 0) {
+                c += " ";
+            }
+            else c += "#";
+        }
+    }
+    console.log(c);
 }
